3422e97266f9b638a89dcd6bb4754fb143ddefbc
Daniel Griffin <dgriffinpv@gmail.com>
2024-02-07 12:55:00 -0700

    added card layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 function App() {
 
   const [cardData, setcardData] = useState([])
-   console.log(cardData)
+  //  console.log(cardData)
 
   useEffect(() => {
     axios.get('/api/cards')
@@ -18,23 +18,28 @@ function App() {
 
   return (
     <div>
-      <section>
-        <label for='nameInput'>Name:</label>
-        <input id='nameInput'></input>
-        <label for='categoryInput'>Category:</label>
-        <input id='categoryInput'></input>
-        <label for='lengthInput'>length in inches:</label>
-        <input id='lengthInput'></input>
-        <label for='colorInput'>Color:</label>
-        <input id='colorInput'></input>
-        <button onClick={()=>{}} >Submit</button>
+      <section className='input-card-container'>
+        <div className='input-card'>
+          <h3>Add a new Animal</h3>
+          <label for='nameInput'>Name:</label>
+          <input id='nameInput'></input>
+          <label for='categoryInput'>Species:</label>
+          <input id='categoryInput'></input>
+          <label for='lengthInput'>length in inches:</label>
+          <input id='lengthInput'></input>
+          <label for='colorInput'>Color:</label>
+          <input id='colorInput'></input>
+          <button className='submit-btn' onClick={()=>{}} >Submit</button>
+        </div>
       </section>
-      <section>
+      <section className='card-container'>
         { cardData.map((species) => {
+          console.log(species)
           return (
             <Card 
               key={species.id}
               species={species}
+
             />
           )
         })}
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,12 @@
 function Card(props) {
+    console.log(props)
     return (
-        <div>
-            <h1>{props.species.name}</h1>
-        </div>
+            <div className="card">
+                <h3>{props.species.name}</h3>
+                <p>Species: {props.species.Category.name}</p>
+                <p>Length: {props.species.length}"</p>
+                <p>Color: {props.species.color}</p>
+            </div>
     )
 }
 
